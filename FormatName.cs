fbba26d3838071cd0e06a3b81b494f93007118c1
Sibby Davy <sibbyc@gmail.com>
2020-08-03 14:47:17 +0530

    Add project files.

---
+++ FormatName.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cbosTestLibaray1
+{
+   public class FormatName
+    {
+        public string GetFormated(string name)
+        {
+            return "hello, " + name;
+        }
+    }
+}
